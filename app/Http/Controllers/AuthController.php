5f179ac8b8b3b07a3aa418b057fa3aa2408e7908
Tamir <tamirrasydi@gmail.com>
2023-06-23 17:25:51 +0700

    add login logout api and token to authenticated the user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,14 +2,43 @@
 
 namespace App\Http\Controllers;
 
+
 use App\Http\Resources\userDetailResource;
 use App\Http\Resources\userResource;
 use App\Models\user;
-
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
+
+    public function login(Request $request){
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+
+        ]);
+
+        $user = user::where('email',$request->email)->first();
+
+        if(! $user || Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' =>['The Provided credential are incorrect.'],
+            ]);
+        }
+
+        return $user->createToken('Laravel')->plainTextToken;
+
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return "anda telah logout";
+    }
+
     public function index()
     {
         $users = user::all();
--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,10 @@ use App\Models\product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
+Route::post('/login',[AuthController::class,'login']);
+Route::get('/logout',[AuthController::class,'logout'])->middleware('auth:sanctum');
 Route::get('/user',[AuthController::class, 'index']);
 Route::get('/user/{id}',[AuthController::class,'detail']);
-Route::get('/product',[ProductController::class,'index']);
+Route::get('/product',[ProductController::class,'index'])->middleware('auth:sanctum');
 Route::get('/product/{id}',[ProductController::class,'show']);
 Route::get('/product/detail/{id}',[ProductController::class,'detail']);
